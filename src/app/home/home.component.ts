2a600717c3c1ff82f0aad9491c8e3a86e28ae2bb
oseunabiola <oluabiolaseun@gmail.com>
2019-11-06 23:22:12 +0100

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,6 @@ export class HomeComponent implements OnInit {
     this.getAllMovies();
   }
   getAllMovies() {
-    return this.movieService.getMovies().subscribe(response => this.allMovies = response.results);
+    return this.movieService.getMovies().subscribe(response => this.allMovies = response);
   }
 }
