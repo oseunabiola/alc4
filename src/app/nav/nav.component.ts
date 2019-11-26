a26d66ec979ed3a415f8cf61baf5a43d5dabae16
oseunabiola <oluabiolaseun@gmail.com>
2019-11-26 22:10:58 +0100

    Improved responsiveness

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -5,11 +5,11 @@ import { routes } from '../routes';
   selector: 'app-nav',
   template: `
     <nav class="row">
-      <div class="col-md-10">
+      <div class="col-md-3">
         <a [routerLink]="['movies']" >Mini-netflix</a>
       </div>
-      <a href="javascript:void(0);" class="icon" onClick="showIcon()"><i class="fa fa-bars"></i></a>
-      <div class="col-md-2" id="navLinks">
+      <a href="javascript:void(0);" class="icon" (click)="showIcon()"><i class="fa fa-bars"></i></a>
+      <div class="col-md-8" id="navLinks">
         <a [routerLink]="['movies']" routerLinkActive="active">Home</a>
         <a [routerLink]="['favorites']" routerLinkActive="active">Favorites</a>
       </div>
@@ -18,18 +18,15 @@ import { routes } from '../routes';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  constructor() {  }
+  ngOnInit() {  }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-}
-
-function showIcon() {
-  const x = document.getElementById('navLinks');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
+  showIcon() {
+    const x = document.querySelector('#navLinks');
+    if(x.style.display === 'block') {
+      x.style.display = 'none';
+    } else {
+      x.style.display = 'block';
+    }
   }
 }
